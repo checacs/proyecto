1adc3a156c74857f8128dd5d489e71ac9d4d1915
checacs <josecarlos.munozferrer@cieep.com>
2025-04-03 20:23:14 +0200

    update

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./componentes/navbar/navbar.component";
 import { FooterComponent } from "./componentes/footer/footer.component";
+import { HeaderComponent } from "./componentes/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
