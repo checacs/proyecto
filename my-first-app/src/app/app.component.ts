902fb02e036a5efdf65bd0333928f3c8463f208a
checacs <josecarlos.munozferrer@cieep.com>
2025-04-03 21:06:47 +0200

    Revert "update"
    This reverts commit 1adc3a156c74857f8128dd5d489e71ac9d4d1915.

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./componentes/navbar/navbar.component";
 import { FooterComponent } from "./componentes/footer/footer.component";
-import { HeaderComponent } from "./componentes/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
