af54af304475c01216d2a9a1e0b2e264cf8d10fe
checacs <josecarlos.munozferrer@cieep.com>
2025-04-08 19:35:11 +0200

    update despues de Bertha

--- my-first-app/src/app/app.routes.ts
+++ my-first-app/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { SobreNosotrosComponent } from './componentes/sobre-nosotros/sobre-nosotros.component';
 import { GaleriaComponent } from './componentes/galeria/galeria.component';
-import { SuscribeteComponent } from './componentes/suscribete/suscribete.component';
+
 import { PageNotFoundComponent } from './componentes/page-not-found/page-not-found.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { ComunidadComponent } from './componentes/comunidad/comunidad.component';
@@ -11,10 +11,11 @@ import { ShopComponent } from './componentes/shop/shop.component';
 export const routes: Routes = [
   {path: 'sobre_nosotros', component: SobreNosotrosComponent},
   {path: 'galeria', component: GaleriaComponent},
-  {path: 'suscribete', component: SuscribeteComponent},
+  {path: 'shop', component: ShopComponent},
+
   {path: 'comunidad', component: ComunidadComponent},
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: '**', component: PageNotFoundComponent},
-  {path: 'shop', component: ShopComponent}
+  {path: '**', component: PageNotFoundComponent}
+
 ];
