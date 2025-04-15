901b6c40062514d088ec5f41358832ed1dca158a
checacs <josecarlos.munozferrer@cieep.com>
2025-04-15 18:37:22 +0200

    update fotos galeria

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -7,11 +7,12 @@ import { HomeComponent } from "./componentes/home/home.component";
 import { ComunidadComponent } from "./componentes/comunidad/comunidad.component";
 import {ShopComponent} from "./componentes/shop/shop.component";
 import { CardComponent } from './componentes/card/card.component';
+import { GaleriaComponent } from "./componentes/galeria/galeria.component";
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent, FooterComponent, HeaderComponent, HomeComponent, ComunidadComponent,
-    ShopComponent, CardComponent],
+    ShopComponent, CardComponent, GaleriaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
