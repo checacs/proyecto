fbc3cb9b7f9fb6e3923d67ad57834d98a315f2be
checacs <josecarlos.munozferrer@cieep.com>
2025-04-16 08:10:45 +0200

    more updates

--- my-first-app/src/app/componentes/shop/shop.component.ts
+++ my-first-app/src/app/componentes/shop/shop.component.ts
@@ -9,10 +9,59 @@ import { CardComponent } from "../card/card.component";
 })
 export class ShopComponent {
   bonsai ={
+    nombre: 'Eleagnus',
+    precio: '500€',
+    variedad: 'Pungens',
+    origen: 'Japon',
+    altura: 'Shohin'
+    }
+  bonsai1 ={
     nombre: 'Junipero',
     precio: '500€',
     variedad: 'Itohigawa',
     origen: 'Japon',
     altura: 'Shohin'
-    }
+      }
+  bonsai2 ={
+    nombre: 'Gardenia',
+    precio: '500€',
+    variedad: 'Jasminoides',
+    origen: 'España',
+    altura: 'Shohin'
+  }
+  bonsai3 ={
+    nombre: 'Chaenomeles',
+    precio: '500€',
+    variedad: 'Japonica',
+    origen: 'Japon',
+    altura: 'Mame'
+  }
+  bonsai4 ={
+    nombre: 'Olmo',
+    precio: '500€',
+    variedad: 'Nirekeyaki',
+    origen: 'España',
+    altura: 'Shohin'
+  }
+  bonsai5 ={
+    nombre: 'Romero',
+    precio: '500€',
+    variedad: 'Rastrero',
+    origen: 'España',
+    altura: 'Shohin'
+  }
+  bonsai6 ={
+    nombre: 'Arce',
+    precio: '500€',
+    variedad: 'Yamamomiji',
+    origen: 'Japon',
+    altura: 'Mame'
+  }
+  bonsai7 ={
+    nombre: 'Pino',
+    precio: '500€',
+    variedad: 'Thunmbergii',
+    origen: 'España',
+    altura: 'Shohin'
+  }
 }
